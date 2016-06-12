42e2cb996c2ea4cb2644ac444f8844967c7c31af
Jamie Burkart <jamie@wowlist.org>
2016-06-12 16:31:59 -0400

    add pastel service add .pastel-border for border-color set up simple dummy app

---
+++ addon/services/pastel.js
@@ -0,0 +1,60 @@
+/*
+import Ember from 'ember';
+
+export default Ember.Service.extend({
+  colors: {
+    'white':              '#eeefec',
+    'pale-orange':        '#fce1d1',
+    'lemon-yellow':       '#f7ec86',
+    'naples-yellow':      '#fae367',
+    'pale-green':         '#beebc7',
+    'yellow':             '#ffdb06',
+    'chrome-yellow':      '#ffd323',
+    'pale-orange-2':      '#fec9b7',
+    'pale-brown':         '#efb884',
+    'yellow-orange':      '#ffb101',
+    'pink':               '#f4a9c5',
+    'yellow-ochre':       '#e9b64e',
+    'salmon-pink':        '#ffa77f',
+    'classic-rose':       '#e2aeb4',
+    'pale-blue':          '#74c2e5',
+    'gray-2':             '#b4b3b1',
+    'pale-vermillion':    '#fe998f',
+    'yellow-gray':        '#bbab8c',
+    'yellow-green':       '#79c039',
+    'gray':               '#a2a5a6',
+    'okre':               '#da8f1f',
+    'orange':             '#f77744',
+    'green':              '#34a63c',
+    'blue':               '#1098d9',
+    'olive-green':        '#949136',
+    'hyacinth-violet':    '#977db5',
+    'green-gray':         '#568e62',
+    'crimson-lake':       '#d45462',
+    'vermillion':         '#e04d38',
+    'blue-green':         '#018c85',
+    'deep-gray':          '#6f7d7e',
+    'rose-madder':        '#d04667',
+    'ultramarine-light':  '#4e75b7',
+    'scarlet':            '#d73d36',
+    'red':                '#cd3031',
+    'brown':              '#a55237',
+    'cobalt-blue':        '#0167b2',
+    'purple':             '#944a83',
+    'dark-gray':          '#5e5f61',
+    'vandyke-brown':      '#76533a',
+    'indian-red':         '#764c42',
+    'violet':             '#624c7f',
+    'deep-green':         '#1f5c3e',
+    'ultramarine':        '#30499a',
+    'prussian-blue-light': '#015482',
+    'prussian-blue':      '#014f94',
+    'dark-carmine':       '#772d24',
+    'dark-green':         '#2b463e',
+    'black':              '#343435'
+  },
+  names: Ember.computed('colors', function(){
+    return Object.keys(this.get('colors'));
+  })
+});
+*/
---
+++ app/services/pastel.js
@@ -0,0 +1,252 @@
+//export { default } from 'ember-oil-pastels/services/pastels';
+import Ember from 'ember';
+
+export default Ember.Service.extend({
+  colors: [
+    {
+      slug: 'white',
+      name: 'White',
+      hex: '#eeefec'
+    },
+    {
+      slug: 'pale-orange',
+      name: 'Pale Orange',
+      hex: '#fce1d1'
+    },
+    {
+      slug: 'lemon-yellow',
+      name: 'Lemon Yellow',
+      hex: '#f7ec86'
+    },
+    {
+      slug: 'naples-yellow',
+      name: 'Naples Yellow',
+      hex: '#fae367'
+    },
+    {
+      slug: 'pale-green',
+      name: 'Pale Green',
+      hex: '#beebc7'
+    },
+    {
+      slug: 'yellow',
+      name: 'Yellow',
+      hex: '#ffdb06'
+    },
+    {
+      slug: 'chrome-yellow',
+      name: 'Chrome Yellow',
+      hex: '#ffd323'
+    },
+    {
+      slug: 'pale-orange-2',
+      name: 'Pale Orange (2)',
+      hex: '#fec9b7'
+    },
+    {
+      slug: 'pale-brown',
+      name: 'Pale Brown',
+      hex: '#efb884'
+    },
+    {
+      slug: 'yellow-orange',
+      name: 'Yellow Orange',
+      hex: '#ffb101'
+    },
+    {
+      slug: 'pink',
+      name: 'Pink',
+      hex: '#f4a9c5'
+    },
+    {
+      slug: 'yellow-ochre',
+      name: 'Yellow Ochre',
+      hex: '#e9b64e'
+    },
+    {
+      slug: 'salmon-pink',
+      name: 'Salmon Pink',
+      hex: '#ffa77f'
+    },
+    {
+      slug: 'classic-rose',
+      name: 'Classic Rose',
+      hex: '#e2aeb4'
+    },
+    {
+      slug: 'pale-blue',
+      name: 'Pale Blue',
+      hex: '#74c2e5'
+    },
+    {
+      slug: 'gray-2',
+      name: 'Gray (2)',
+      hex: '#b4b3b1'
+    },
+    {
+      slug: 'pale-vermillion',
+      name: 'Pale Vermillion',
+      hex: '#fe998f'
+    },
+    {
+      slug: 'yellow-gray',
+      name: 'Yellow Gray',
+      hex: '#bbab8c'
+    },
+    {
+      slug: 'yellow-green',
+      name: 'Yellow Green',
+      hex: '#79c039'
+    },
+    {
+      slug: 'gray',
+      name: 'Gray',
+      hex: '#a2a5a6'
+    },
+    {
+      slug: 'okre',
+      name: 'Okre',
+      hex: '#da8f1f'
+    },
+    {
+      slug: 'orange',
+      name: 'Orange',
+      hex: '#f77744'
+    },
+    {
+      slug: 'green',
+      name: 'Green',
+      hex: '#34a63c'
+    },
+    {
+      slug: 'blue',
+      name: 'Blue',
+      hex: '#1098d9'
+    },
+    {
+      slug: 'olive-green',
+      name: 'Olive Green',
+      hex: '#949136'
+    },
+    {
+      slug: 'hyacinth-violet',
+      name: 'Hyacinth Violet',
+      hex: '#977db5'
+    },
+    {
+      slug: 'green-gray',
+      name: 'Green Gray',
+      hex: '#568e62'
+    },
+    {
+      slug: 'crimson-lake',
+      name: 'Crimson Lake',
+      hex: '#d45462'
+    },
+    {
+      slug: 'vermillion',
+      name: 'Vermillion',
+      hex: '#e04d38'
+    },
+    {
+      slug: 'blue-green',
+      name: 'Blue Green',
+      hex: '#018c85'
+    },
+    {
+      slug: 'deep-gray',
+      name: 'Deep Gray',
+      hex: '#6f7d7e'
+    },
+    {
+      slug: 'rose-madder',
+      name: 'Rose Madder',
+      hex: '#d04667'
+    },
+    {
+      slug: 'ultramarine-light',
+      name: 'Ultramarine Light',
+      hex: '#4e75b7'
+    },
+    {
+      slug: 'scarlet',
+      name: 'Scarlet',
+      hex: '#d73d36'
+    },
+    {
+      slug: 'red',
+      name: 'Red',
+      hex: '#cd3031'
+    },
+    {
+      slug: 'brown',
+      name: 'Brown',
+      hex: '#a55237'
+    },
+    {
+      slug: 'cobalt-blue',
+      name: 'Cobalt Blue',
+      hex: '#0167b2'
+    },
+    {
+      slug: 'purple',
+      name: 'Purple',
+      hex: '#944a83'
+    },
+    {
+      slug: 'dark-gray',
+      name: 'Dark Gray',
+      hex: '#5e5f61'
+    },
+    {
+      slug: 'vandyke-brown',
+      name: 'Vandyke Brown',
+      hex: '#76533a'
+    },
+    {
+      slug: 'indian-red',
+      name: 'Indian Red',
+      hex: '#764c42'
+    },
+    {
+      slug: 'violet',
+      name: 'Violet',
+      hex: '#624c7f'
+    },
+    {
+      slug: 'deep-green',
+      name: 'Deep Green',
+      hex: '#1f5c3e'
+    },
+    {
+      slug: 'ultramarine',
+      name: 'Ultramarine',
+      hex: '#30499a'
+    },
+    {
+      slug: 'prussian-blue-light',
+      name: 'Prussian Blue Light',
+      hex: '#015482'
+    },
+    {
+      slug: 'prussian-blue',
+      name: 'Prussian Blue',
+      hex: '#014f94'
+    },
+    {
+      slug: 'dark-carmine',
+      name: 'Dark Carmine',
+      hex: '#772d24'
+    },
+    {
+      slug: 'dark-green',
+      name: 'Dark Green',
+      hex: '#2b463e'
+    },
+    {
+      slug: 'black',
+      name: 'Black',
+      hex: '#343435'
+    }
+  ]
+});
